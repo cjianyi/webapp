092e96dd619cac3e6eab45bfe722c725b32211a3
Jianyi Chen <williamchen33333@gmail.com>
2021-06-20 00:48:48 -0700

    created search page

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
 from flask import Flask, render_template
 from flaskext.mysql import MySQL
+from database import connect
 
-
+    
 app = Flask(__name__)
- 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'patients'
-mysql = MySQL(app)
-
-
-conn = mysql.connect()
-cur = conn.cursor()
-#Executing SQL Statements
-cur.execute(''' CREATE TABLE patients (id INTEGER, name VARCHAR(255)
- ''')
-conn.commit()
-cur.close()
- 
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
+
+@app.route('/search')
+def search():
+    mysql = connect()
+    conn = mysql.connect()
+    cur = conn.cursor()
+    #Executing SQL Statements
+    cur.execute(''' SELECT count(*) FROM patients''')
+    conn.commit()
+    cur.close()
+    return render_template('search.html')
 
 if __name__ == '__main__':
     app.run()
