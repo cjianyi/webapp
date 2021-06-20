5190bc27b420391da1db26ef1b933714c0129b72
Jianyi Chen <williamchen33333@gmail.com>
2021-06-20 00:49:13 -0700

    added database file

--- database.py
+++ database.py
@@ -1,3 +1,40 @@
 from flaskext.mysql import MySQL
+from flask import Flask, render_template
 
-mySQL = MySQL()
+
+
+def connect():
+    """
+    Connect to the database and create table
+    """
+    app = Flask(__name__)
+        
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_DB'] = 'patients'
+    mysql = MySQL(app)
+    return mysql
+
+
+
+def createTable():
+    """
+    Create table patients
+    """
+    app = Flask(__name__)
+        
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_DB'] = 'patients'
+    mysql = MySQL(app)
+
+
+    conn = mysql.connect()
+    cur = conn.cursor()
+    #Executing SQL Statements
+    cur.execute(''' CREATE TABLE patients (id INTEGER, name VARCHAR(255)
+    ''')
+    conn.commit()
+    cur.close()
